40a1208e4e87266c4432e466fc61f6074964d3a9
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-12-25 23:06:24 +0600

    added my some work

--- index.js
+++ index.js
@@ -135,6 +135,12 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", varifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
