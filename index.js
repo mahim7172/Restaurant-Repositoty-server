a240ba09a2713d13eeec0129ce5d02ea96dcceba
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2023-12-28 00:37:45 +0600

    added my payment method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRIET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -189,7 +190,21 @@ async function run() {
       const result = await cardCollection.deleteOne(query);
       res.send(result);
     });
+    // payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
 
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     // console.log(
